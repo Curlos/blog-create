bbcef22e526f6e5af5b8b002e928b6d161ea741c
Carlos Martinez <curlosmart@gmail.com>
2021-10-08 20:40:48 -0400

    user reducer added

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,35 @@
+import userService from '../services/users'
+
+const userReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      console.log(action.data)
+      return action.data
+    case 'LOGOUT':
+      return action.data
+    default:
+      return state
+  }
+}
+
+export const logInUser = (userInfo) => {
+  return async dispatch => {
+    const user = await userService.logInUser(userInfo)
+    dispatch({
+      type: 'LOGIN',
+      data: user
+    })
+  }
+}
+
+export const logOutUser = () => {
+  return async dispatch => {
+    const user = await userService.logOutUser()
+    dispatch({
+      type: 'LOGOUT',
+      data: user
+    })
+  }
+}
+
+export default userReducer;
