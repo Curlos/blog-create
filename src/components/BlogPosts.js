b51c7f7f85530d42bdd13ba8b16f74341892c685
Carlos Martinez <curlosmart@gmail.com>
2021-10-10 15:56:20 -0400

    Get author from backend for thumbnail

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getAllBlogPosts } from '../reducers/blogPostReducer'
+import userService from '../services/users'
 
 const SmallBlogPost = ({ blogPost }) => {
+
+  const [blogPostAuthor, setBlogPostAuthor] = useState({})
+
+  useEffect(() => {
+    const getAuthor = async () => {
+      const data = await userService.getUser(blogPost.author) 
+      console.log(data)
+      setBlogPostAuthor(data)
+    }
+
+    getAuthor()
+  }, [])
+
+  /*
+
+  <div className="blogPostPreviewText">
+            {blogPost.text.length < 280 ? blogPost.text.slice(0, 280) :
+            (
+              blogPost.text.slice(0, 280) + '...'
+            )}
+          </div>
+
+  */
+
   return (
       <Link to={`/blog-post/${blogPost._id}`} className="blogPostThumbnail">
         <div>
           <div className="blogPostTitle">{blogPost.title}</div>
           <div>
-            <span className="blogPostAuthor">{blogPost.author}</span>
+            <span className="blogPostAuthor">{blogPostAuthor.fullName || blogPostAuthor.username}</span>
             <i class="fas fa-comment"></i> {blogPost.comments.length}
           </div>
+          
         </div>
         <div>
-          <img src="https://cdn.theathletic.com/cdn-cgi/image/quality=85,height=800/app/uploads/2021/10/08082532/GettyImages-1313693991-1024x683.jpg" alt={blogPost.title} className="blogPostImage"/>
+          <img src={blogPost.headerImageURL || "https://cdn.theathletic.com/cdn-cgi/image/quality=85,height=800/app/uploads/2021/10/08082532/GettyImages-1313693991-1024x683.jpg"} alt={blogPost.title} className="blogPostImage"/>
         </div>
       </Link>
   )
